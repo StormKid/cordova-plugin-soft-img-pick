8d622e20c92dbef2ea18247ca923aeebaf0376a9
ke_li <stokid@126.com>
2017-11-30 15:18:31 +0800

    修改js语法问题

--- www/softImgPick.js
+++ www/softImgPick.js
@@ -19,25 +19,25 @@ exports.showList = function(done, err, options) {
 
     var params = {
         //确定按钮文字
-        ok_text = options.ok_text ? options.ok_text : "确定",
+        ok_text: options.ok_text ? options.ok_text : "确定",
         //返回按钮文字
-        cancel_text = options.cancel_text ? options.cancel_text : "返回",
+        cancel_text: options.cancel_text ? options.cancel_text : "返回",
         //勾选小图标
-        check_img_res = options.check_img_res ? options.check_img_res : "",
+        check_img_res: options.check_img_res ? options.check_img_res : "",
         //确定和返回字的大小
-        manager_title_text_size = options.manager_title_text_size ? options.manager_title_text_size : 10,
+        manager_title_text_size: options.manager_title_text_size ? options.manager_title_text_size : 10,
         //限制相册选择数量
-        limit_num = options.limit_num ? options.limit_num : 9,
+        limit_num: options.limit_num ? options.limit_num : 9,
         //是否启动网络请求上传数据
-        post_imgs = options.post_imgs ? options.post_imgs : true,
+        post_imgs: options.post_imgs ? options.post_imgs : true,
         //启动网络请求过后指定url
-        path_url = options.path_url ? options.path_url : "",
+        path_url: options.path_url ? options.path_url : "",
         //文件上传默认的key
-        file_key = options.file_key ? options.file_key : "files",
+        file_key: options.file_key ? options.file_key : "files",
         //文件上传的参数
-        photo_params = options.photo_params ? options.photo_params : {},
+        photo_params: options.photo_params ? options.photo_params : {},
         //顶部banner的颜色
-        banner_color = options.banner_color ? options.banner_color : "ffffff"
+        banner_color: options.banner_color ? options.banner_color : "ffffff"
     }
     argscheck.checkArgs("FFO", "softImgPick.showList", arguments)
     return exec(done, err, "softImgPick", "showList", [params])
